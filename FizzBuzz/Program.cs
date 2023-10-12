d3c3253e197757a671c4817fbb93038eaa40c480
markus.wuethrich <markus.wuethrich@erowa.com>
2023-10-12 11:20:52 +0200

    Add explanations

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,7 +1,26 @@
 ﻿using FizzBuzz;
 using FizzBuzz.Impl;
 
-var console = new ConsoleWrapper();
-var fizzBuzzer = new FizzBuzzer(console);
+ConsoleWrapper console = new ConsoleWrapper();
 
+int rest = 4 % 3;
+console.WriteLine("Ein Rest wird mit '%' gerechnet: 4 durch 3 gibt Rest " + rest);
+
+return;
+
+console.WriteLine("\n--------------------------***--------------------------\n");
+
+console.WriteLine("Ein Kreislauf (loop) wird mit 'while' gemacht: ");
+int n = 1;
+int max = 8;
+while (n < max)
+{
+    console.WriteLine("Wir sind zum " + n + "-ten mal im loop.");
+    n = n + 1;
+}
+
+console.WriteLine("\n--------------------------***--------------------------\n");
+
+
+FizzBuzzer fizzBuzzer = new FizzBuzzer(console);
 fizzBuzzer.FizzBuzz(1);
